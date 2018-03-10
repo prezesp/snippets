e2e8861163970b5d7eca7a42df11457188b3d40c
Piotr Walkiewicz <piotr.walkiewicz@yahoo.com>
2018-03-10 19:16:45 +0100

    Add course progress block

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-// Konfiguracja
 var config = {
-	title: "Nauka SQL",
-	subtitle: "Krótki kurs o tym i o tamtym",
 	url: "data/snippets.json",
-	lang: "sql",
 	show_immediately: false
 }
 
-// Zapisywanie kliknięć w storage
+/** 
+  * @desc Save course progress in local storage.
+  * @param int v_id - unique identifier of course step.
+  * @param bool v_state - indicates step is accomplished or not.
+*/
 function saveSnippets(v_id, v_state) {
 	var sn = top.localStorage.getItem('snippets-notebook') || '[]';
 	sn = JSON.parse(sn).filter(function(element) {
@@ -18,6 +18,10 @@ function saveSnippets(v_id, v_state) {
 		state: v_state
 	});
 	top.localStorage.setItem('snippets-notebook', JSON.stringify(sn));
+
+	// update score
+	var $score = $('.score');
+	$score.find('.panel-body span:nth-child(1)').html(sn.filter(s => s.state).length);
 };
 
 function loadSnippets() {
@@ -25,10 +29,13 @@ function loadSnippets() {
 	sn = JSON.parse(sn).filter(function(element) {
 		return element.state == true;
 	}).map(a => a.id);
+
+	// update score
+	var $score = $('.score');
+	$score.find('.panel-body span:nth-child(1)').html(sn.length);
 	return sn;
 }
 
-// Zmiana klasy dla buttona
 function toggleClass(b, id) {
 	console.log($(b));
 	var mark = $(b).hasClass('btn-secondary');
@@ -48,23 +55,30 @@ $( document ).ready(function() {
 		location.reload();
 	});
 	var $container = $('.container:first');
-	$container.find('h2:first').html(config.title);
-	$container.find('p:first').html(config.subtitle);
+	var $score = $('.score');
 	
-	// Pobanie snippetów z okreslonego zasobu (url)
+	// Get snippets from predefined url
 	$.get({
 		url: config.url,
-		success: function(snippets) {
+		success: function(data) {
+
+			// Apply metadata to view
+			$container.find('h2:first').html(data.title);
+			$container.find('p:first').html(data.subtitle);
+			$score.find('.panel-body span:nth-child(2)').html(data.snippets.length);
+
+			// Render snippets
 			var completedSnippets = loadSnippets();
-			snippets.forEach(function(s, i) {
+			data.snippets.forEach(function(s, i) {
 				var $pointDiv = $('<div/>', {
 					'class': 'row point',	
 					'html': '<div class="col-lg-offset-1 col-lg-10" style="float:unset !important"><h4>' + (i+1) + '. ' + s.title + '</h4></div>'
 				}).appendTo($container);
+
 				s.fragments.forEach(function(fragment, j){
 					$('<div/>', {
 						'class': 'col',
-						'html': '<p>' + (fragment.text || '') + '</p><div class="card"><div class="card-body"><pre><code class="' + config.lang + '">' + (fragment.code || '') + '</code></pre></div></div>'
+						'html': '<p>' + (fragment.text || '') + '</p><div class="card"><div class="card-body"><pre><code class="' + data.lang + '">' + (fragment.code || '') + '</code></pre></div></div>'
 					}).appendTo($pointDiv.find("div:first"));
 					var snippet_id = i + '_' + j;
 					$('<div/>', {
@@ -75,13 +89,13 @@ $( document ).ready(function() {
 				
 			});
 
-			// Wyświetlanie punktów przy scrollowaniu
+			// Use scrollreveal
 			if (!config.show_immediately) {
 				window.sr = ScrollReveal({ reset: false, viewFactor : 0.7 });
 				sr.reveal('.point');
 			}
 
-			// Ustawienie kolorwania składni
+			// Syntax highlighting
 			hljs.initHighlighting();
 		}
 	})
